d3353b67ae2f5eb32e062bd30f447c9543d55515
Jordan Thompson <jordanthomp81@gmail.com>
2018-09-20 20:56:17 -0500

    Removed hard coding of terrain x and z position.

--- populateSpawns.cs
+++ populateSpawns.cs
@@ -5,46 +5,45 @@ using UnityEngine;
 public class populateSpawns : MonoBehaviour
 {
 
-    [SerializeField]
-    private GameObject spawnPrefab;
-    private MeshCollider meshCollider;
+	[SerializeField]
+	private GameObject spawnPrefab;
+	private MeshCollider meshCollider;
 
-    // Gather the bounding box vector positions needed to generate the random item locations
-    // Serialize the x,y, and z axis values to use for random number generation
-    [SerializeField]
-    private float spawnTerrainXMin;
-    [SerializeField]
-    private float spawnTerrainXMax;
-    [SerializeField]
-    private float spawnTerrainZMin;
-    [SerializeField]
-    private float spawnTerrainZMax;
-    [SerializeField]
-    private int totalSpawnMinimum = 12;
+	// Gather the bounding box vector positions needed to generate the random item locations
+	// Serialize the x,y, and z axis values to use for random number generation
+	private float spawnTerrainXMin;
+	private float spawnTerrainXMax;
+	private float spawnTerrainZMin;
+	private float spawnTerrainZMax;
+	[SerializeField]
+	private int totalSpawnMinimum = 12;
 
-    private float spawnArea = 100f;
-    public static int totalSpawns;
+	public static int totalSpawns;
 
-    // Update is called once per frame
-    void Awake()
-    {
-        meshCollider = GetComponent<MeshCollider>();
-        totalSpawns = 0;
+	// Update is called once per frame
+	void Awake()
+	{
+		meshCollider = GetComponent<MeshCollider>();
+		spawnTerrainXMin = meshCollider.transform.position.x;
+		spawnTerrainXMax = spawnTerrainXMin + 25;
+		spawnTerrainZMin = meshCollider.transform.position.z;
+		spawnTerrainZMax = spawnTerrainZMin + 25;
+		totalSpawns = 0;
 
-        while (totalSpawns < totalSpawnMinimum)
-        {
-            float maxHeight = 20f;
-            float positionX = Random.Range(spawnTerrainXMin, spawnTerrainXMax);
-            float positionZ = Random.Range(spawnTerrainZMin, spawnTerrainZMax);
+		while (totalSpawns < totalSpawnMinimum)
+		{
+			float maxHeight = 20f;
+			float positionX = Random.Range(spawnTerrainXMin, spawnTerrainXMax);
+			float positionZ = Random.Range(spawnTerrainZMin, spawnTerrainZMax);
 
-            Ray ray = new Ray(new Vector3(positionX, maxHeight, positionZ), Vector3.down);
-            RaycastHit hit;
-            if (meshCollider.Raycast(ray, out hit, maxHeight))
-            {
-                Vector3 spawnPosition = new Vector3(positionX, hit.point.y, positionZ);
-                Instantiate(spawnPrefab, spawnPosition, Quaternion.identity);
-            }
-            totalSpawns++;
-        }
-    }
+			Ray ray = new Ray(new Vector3(positionX, maxHeight, positionZ), Vector3.down);
+			RaycastHit hit;
+			if (meshCollider.Raycast(ray, out hit, maxHeight))
+			{
+				Vector3 spawnPosition = new Vector3(positionX, hit.point.y, positionZ);
+				Instantiate(spawnPrefab, spawnPosition, Quaternion.identity);
+			}
+			totalSpawns++;
+		}
+	}
 }
