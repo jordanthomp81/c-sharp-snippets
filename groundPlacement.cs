9da88ce519f4a83d6c3794c88bf7ced64e1b79a5
Jordan Thompson <jordanthomp81@gmail.com>
2018-09-29 18:58:10 -0500

    added file to allow users to place buildings from a gui button

---
+++ groundPlacement.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+
+public class groundPlacement_v2 : MonoBehaviour
+{
+
+	[SerializeField]
+	private GameObject placeableObjectPrefab;
+	private GameObject currentPlaceableObject;
+	private float mouseWheelRotation;
+
+	private void Update()
+	{
+		if (currentPlaceableObject != null)
+		{
+			MoveCurrentObjectToMouse();
+			RotateFromMouseWheel();
+			ReleaseIfClicked();
+		}
+	}
+
+	private void RotateFromMouseWheel()
+	{
+		mouseWheelRotation += Input.mouseScrollDelta.y;
+		currentPlaceableObject.transform.Rotate(Vector3.up, mouseWheelRotation * 10f);
+	}
+
+	private void MoveCurrentObjectToMouse()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		RaycastHit hitInfo;
+		if (Physics.Raycast(ray, out hitInfo))
+		{
+			currentPlaceableObject.transform.position = hitInfo.point;
+			currentPlaceableObject.transform.rotation = Quaternion.FromToRotation(Vector3.up, hitInfo.normal);
+		}
+	}
+
+	private void ReleaseIfClicked()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			currentPlaceableObject = null;
+		}
+	}
+
+	public void HandleNewObjectHotkey()
+	{
+		if (currentPlaceableObject != null)
+		{
+			Destroy(currentPlaceableObject);
+		}
+		else
+		{
+			currentPlaceableObject = Instantiate(placeableObjectPrefab);
+		}
+	}
+}
