66110296dd8df574d0b38e69767ac65cddd7292d
Jordan Thompson <jordanthomp81@gmail.com>
2018-10-01 20:09:54 -0500

    added logic handling for buildings

--- gameManager.cs
+++ gameManager.cs
@@ -8,6 +8,8 @@ public class gameManager : MonoBehaviour {
 	public static GameObject currSelectedBuilding;
 
 	[SerializeField]
+	private Canvas cityCenterUI;
+	[SerializeField]
 	private GameObject spawnPrefab;
 	private GameObject currentPlaceableObject;
 	private int spawnFlagCount = 0;
@@ -16,6 +18,29 @@ public class gameManager : MonoBehaviour {
 	void Update () {
 		if (buildingSelected == true)
 		{
+			if (Input.GetMouseButtonDown(0))
+			{
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+				RaycastHit hitInfo;
+				if (Physics.Raycast(ray, out hitInfo))
+				{
+					if(currSelectedBuilding.name != hitInfo.transform.name)
+					{
+						currSelectedBuilding.GetComponent<Outline>().enabled = !currSelectedBuilding.GetComponent<Outline>().enabled;
+						if (currSelectedBuilding.GetComponent<Outline>().enabled == false)
+						{
+							// hide ui
+							cityCenterUI.GetComponent<Canvas>().enabled = false;
+							buildingSelected = false;
+							currSelectedBuilding.GetComponent<buildingClick>().updateSpawnLocation();
+							Destroy(GameObject.Find("shovel(Clone)"));
+							Destroy(currentPlaceableObject);
+						}
+					}
+				}
+			}
+
 			if (Input.GetMouseButtonDown(1))
 			{
 				Destroy(GameObject.Find("shovel(Clone)"));
