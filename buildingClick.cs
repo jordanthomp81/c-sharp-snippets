9e8c33932ef9c804e5e514e5eeb3ac9b07247bc1
Jordan Thompson <jordanthomp81@gmail.com>
2020-03-15 21:55:35 -0500

    streamline work; broke logic into separate functions

--- buildingClick.cs
+++ buildingClick.cs
@@ -1,33 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class buildingClick : MonoBehaviour {
+namespace cakeslice
+{
+    public class buildingClick : MonoBehaviour
+    {
+        private GameObject BuildingUI;
+        private GameObject BuildingUIText;
 
-	[SerializeField]
-	private GameObject citizenPrefab;
-	private GameObject building;
+        void Start()
+        {
+            BuildingUI = GameObject.Find("UI").transform.GetChild(0).gameObject;
+            BuildingUIText = GameObject.Find("UI/BuildingUI/Panel/Text");
+        }
 
-	// Use this for initialization
-	void OnMouseDown()
-	{
-		gameManager.buildingSelected = true;
+        private void selectBuilding()
+        {
+            gameManager.buildingSelected = true;
+            gameManager.currSelectedBuilding = this.gameObject;
+        }
 
-		if (Input.GetMouseButtonDown(0))
-		{
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
+        private void deselectBuilding()
+        {
+            gameManager.buildingSelected = false;
+            gameManager.currSelectedBuilding = null;
+        }
 
-			if (Physics.Raycast(ray, out hit, 100))
-			{
-				building = GameObject.Find(hit.transform.gameObject.name);
-				gameManager.currSelectedBuilding = building;
-				GetComponent<Outline>().enabled = !GetComponent<Outline>().enabled;
-				if(GetComponent<Outline>().enabled == false)
-				{
-					gameManager.buildingSelected = false;
-				}
-			}
-		}
-	}
+        private void updateBuildingUI(string text)
+        {
+            BuildingUIText.GetComponent<Text>().text = text;
+        }
+
+        private void toggleOutline(bool currGameObj)
+        {
+            // are we using the curr game object or a reference to the gamemanger saved gameobject??
+            if(currGameObj)
+            {
+                GetComponent<Outline>().enabled = !GetComponent<Outline>().enabled;
+            }
+            else
+            {
+                gameManager.currSelectedBuilding.GetComponent<Outline>().enabled = false;
+            }
+        }
+
+        void OnMouseDown()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                // check if there is already a building highlighted & resolve
+                if (gameManager.currSelectedBuilding != null)
+                {
+                    toggleOutline(false);
+                }
+
+                // check curr game object outline state to determine course of action
+                if (GetComponent<Outline>().enabled == true)
+                {
+                    deselectBuilding();
+                    updateBuildingUI("");
+                }
+                else
+                {
+                    selectBuilding();
+                    updateBuildingUI(this.gameObject.transform.name);
+                }
+
+                toggleOutline(true);
+            }
+        }
+    }
 }
